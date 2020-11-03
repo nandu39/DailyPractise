a5d48d912de44b19a62bb53d72c46fbc55459dd9
nandu <nanduambati@gmail.com>
2020-11-03 14:19:59 +0530

    Added Apsrtc Login Signup

---
+++ src/main/java/nanduselenium/Google.java
@@ -0,0 +1,22 @@
+package nanduselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Google
+{
+    public static void main(String[] args)
+    {
+
+        System.setProperty("webdriver.chrome.driver", "D:\\Selenium_Softwares\\chromedriver.exe");
+
+        WebDriver driver=new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+
+        System.out.println(driver.getTitle());
+
+        driver.close();
+
+    }
+}
