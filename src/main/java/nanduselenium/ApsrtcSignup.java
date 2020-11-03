1706f4012ee63fcfb9fb7a7f375c8af719175b64
nandu <nanduambati@gmail.com>
2020-11-03 15:39:31 +0530

    updated

--- src/main/java/nanduselenium/ApsrtcSignup.java
+++ src/main/java/nanduselenium/ApsrtcSignup.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class ApsrtcSignup
 {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "D:\\Selenium_Softwares\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
 
         WebDriver driver=new ChromeDriver();
 
